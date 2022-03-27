6c6ff43aa09cdd186c0f209842bd6fc660391f87
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-03-27 12:05:27 +0900

    withbase changed

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git init
 git add -A
 git commit -m 'deploy with vuepress'
 
-git push -f https://github.com/pullingoff/vue-til.git master:gh-pages
+git push -f https://github.com/pullingoff/vue-til.git main:gh-pages
 
 
 cd -
